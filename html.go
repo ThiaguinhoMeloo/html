7c022bfa362bf2b96b81413b2ae18eb5e4bdc5bb
Thiago Melo <jamesmelo329@gmail.com>
2024-07-26 15:47:48 -0300

    fix: alter html.go

--- html.go
+++ html.go
@@ -1,6 +1,7 @@
 package html
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"regexp"
@@ -11,7 +12,11 @@ func Title(urls ...string) <-chan string {
 	c := make(chan string)
 	for _, url := range urls {
 		go func(url string) {
-			resp, _ := http.Get(url)
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Println(err.Error())
+				c <- ""
+			}
 			html, _ := io.ReadAll(resp.Body)
 			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
 			c <- r.FindStringSubmatch(string(html))[1]
