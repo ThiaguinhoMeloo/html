f1eb39ef72cbe365f2f3085f99fa4f1ac8231e0f
Thiago Melo <jamesmelo329@gmail.com>
2024-07-26 15:31:31 -0300

    commit 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ThiaguinhoMeloo/html
+
+go 1.22.5
---
+++ html.go
@@ -0,0 +1,22 @@
+package html
+
+import (
+	"io"
+	"net/http"
+	"regexp"
+)
+
+// Titulo obtem o título de uma pagina html
+func Title(urls ...string) <-chan string {
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := io.ReadAll(resp.Body)
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			c <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+
+	return c
+}
